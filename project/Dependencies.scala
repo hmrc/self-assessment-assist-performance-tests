d6ee3d7895ada91b0b1d923ae6df7431bacb0842
stephenkward <15212062+stephenkward@users.noreply.github.com>
2025-11-14 15:24:05 +0000

    performance-test-runner update

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,9 +3,9 @@ import sbt._
 object Dependencies {
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"          %% "performance-test-runner"   % "6.1.0",
-    "org.apache.commons"    % "commons-lang3"             % "3.17.0",
-    "com.typesafe"          % "config"                    % "1.4.3"
+    "uk.gov.hmrc"          %% "performance-test-runner"   % "6.2.0",
+    "org.apache.commons"    % "commons-lang3"             % "3.19.0",
+    "com.typesafe"          % "config"                    % "1.4.5"
   ).map(_ % Test)
 
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -4,5 +4,5 @@ resolvers += Resolver.url("HMRC-open-artefacts-ivy2", url("https://open.artefact
 )
 
 addSbtPlugin("uk.gov.hmrc"   % "sbt-auto-build" % "3.24.0")
-addSbtPlugin("io.gatling"    % "gatling-sbt"    % "4.9.2")
-addSbtPlugin("org.scalameta" % "sbt-scalafmt"   % "2.4.0")
+addSbtPlugin("io.gatling"    % "gatling-sbt"    % "4.15.0")
+addSbtPlugin("org.scalameta" % "sbt-scalafmt"   % "2.5.5")
