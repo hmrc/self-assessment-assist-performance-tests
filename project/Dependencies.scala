94880b17cbe76c98bd8e21a8ea826a7ba7bd5db5
stephenkward <15212062+stephenkward@users.noreply.github.com>
2025-11-14 15:30:22 +0000

    performance-test-runner to 6.3.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt.*
 object Dependencies {
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"          %% "performance-test-runner"   % "6.2.0",
+    "uk.gov.hmrc"          %% "performance-test-runner"   % "6.3.0",
   ).map(_ % Test)
 
 }
