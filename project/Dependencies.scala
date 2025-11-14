3997a32ca033baf9c32273775b24019473c60d7b
stephenkward <15212062+stephenkward@users.noreply.github.com>
2025-11-14 15:26:50 +0000

    Removed unused dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,11 +1,9 @@
-import sbt._
+import sbt.*
 
 object Dependencies {
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"          %% "performance-test-runner"   % "6.2.0",
-    "org.apache.commons"    % "commons-lang3"             % "3.19.0",
-    "com.typesafe"          % "config"                    % "1.4.5"
   ).map(_ % Test)
 
 }
